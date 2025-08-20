1b7d7847c45754628dc954e88a0e9f46340d29cc
kcharan <saic3076@gmail.com>
2025-08-20 13:45:06 +0530

    basic restapi

---
+++ 3.restful_service/handlers/products.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+	"restapi/data"
+)
+
+type Products struct {
+	l *log.Logger
+}
+
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
+	lp := data.GetProducts()
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Failed to marshal products", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.GetProducts(rw, r)
+		return
+	}
+	http.Error(rw, "Method not allowed", http.StatusMethodNotAllowed)
+}
