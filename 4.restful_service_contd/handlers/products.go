c3233699153af27f5e3ce24f95d4337ba65c9646
kcharan <saic3076@gmail.com>
2025-08-23 09:50:34 +0530

    implementing post, update methods

---
+++ 4.restful_service_contd/handlers/products.go
@@ -0,0 +1,83 @@
+package handlers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+	"restapi/data"
+	"strconv"
+)
+
+type Products struct {
+	l *log.Logger
+}
+
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
+	fmt.Println("Handle GET Products")
+	lp := data.GetProducts()
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Failed to marshal products", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (p *Products) addProducts(rw http.ResponseWriter, r *http.Request) {
+	fmt.Println("Handle POST Product")
+	var prod data.Product
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to UnMarshal", http.StatusBadRequest)
+		return
+	}
+	data.AddProduct(&prod)
+}
+
+func (p *Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	fmt.Println("Handle PUT Product")
+	var prod data.Product
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to UnMarshal", http.StatusBadRequest)
+		return
+	}
+	err = prod.UpdateProduct(id)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusNotFound)
+		return
+	}
+}
+
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.GetProducts(rw, r)
+		return
+	}
+	if r.Method == http.MethodPost {
+		p.addProducts(rw, r)
+		return
+	}
+	if r.Method == http.MethodPut {
+		ptm := regexp.MustCompile(`/([0-9])`)
+		g := ptm.FindAllStringSubmatch(r.URL.Path, -1)
+		if len(g) > 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		p.l.Println("gotID: ", id)
+		p.updateProducts(id, rw, r)
+		return
+	}
+	http.Error(rw, "Method not allowed", http.StatusMethodNotAllowed)
+}
---
+++ 4.restful_service_contd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"restapi/handlers"
+	"syscall"
+	"time"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hp := handlers.NewProducts(l)
+
+	mu := http.NewServeMux()
+
+	mu.HandleFunc("/", hp.ServeHTTP)
+	s := http.Server{
+		Addr:         ":9090",
+		Handler:      mu,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+	<-sigChan
+	l.Println("Received shutdown signal, shutting down server...")
+	tc, cancelFunc := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancelFunc()
+	s.Shutdown(tc)
+}
